ebcc6b372a8468035ef25caec4c21aa996915721
Mayank <mayankanakiya.efive@outlook.com>
2025-05-15 11:10:19 +0530

    test-core project initiated

---
+++ src/main/java/com/test_core/TestCoreApplication.java
@@ -0,0 +1,15 @@
+package com.test_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCoreApplication.class, args);
+		
+		
+	}
+
+}
